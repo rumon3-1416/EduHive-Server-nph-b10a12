1b7e320bab5e6be207817c20b33d26647094a99e
s-h-rumon <shahidhasanruman3@gmil.com>
2025-01-14 13:10:34 +0600

    get classes done

--- controllers/getController.js
+++ controllers/getController.js
@@ -1,8 +1,28 @@
 const { tryCatch } = require('../utils/tryCatch');
 
+// Banner Slides
 const getSlides = tryCatch(async (req, res, collection) => {
   const result = await collection.find().toArray();
   res.send(result);
 });
 
-module.exports = { getSlides };
+// Popular Classes
+const getClasses = async (req, res, collection) => {
+  const { popular, limit } = req.query;
+
+  const filter = {
+    status: 'approved',
+  };
+  const sortQuery = popular ? { total_enrolment: -1 } : {};
+  const limitQuery = limit ? parseInt(limit) : 0;
+
+  const result = await collection
+    .find(filter)
+    .sort(sortQuery)
+    .limit(limitQuery)
+    .toArray();
+
+  res.send(result);
+};
+
+module.exports = { getSlides, getClasses };
--- index.js
+++ index.js
@@ -2,13 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 // Token
 const { postJwtToken, clearJwtToken } = require('./controllers/jwtController');
 // Middlewares
 
 // Controllers
-const { getSlides } = require('./controllers/getController');
+const { getSlides, getClasses } = require('./controllers/getController');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -22,6 +23,7 @@ const corsOption = {
 app.use(cors(corsOption));
 app.use(express.json());
 app.use(cookieParser());
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send('Hello');
@@ -42,17 +44,25 @@ let isConnected = false;
     !isConnected &&
       (await client.connect(), (isConnected = true), console.log('Connected'));
     const database = client.db('a12DB');
+
     // Collections
     const slideCollection = database.collection('banner_slides');
+    const classCollection = database.collection('classes');
 
     // Jwt Token
     app.post('/jwt', postJwtToken);
     app.post('/logout', clearJwtToken);
 
     // Get
+    // Banner Slides
     app.get(
       '/slides',
       async (req, res) => await getSlides(req, res, slideCollection)
+    );
+    // Classes
+    app.get(
+      '/classes',
+      async (req, res) => await getClasses(req, res, classCollection)
     );
   } catch (error) {
     console.log(error.message);
